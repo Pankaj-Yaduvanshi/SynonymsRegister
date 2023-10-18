21929b979db60af99cff3b076a89c4dc7d7ef31d
Pankaj-Yaduvanshi <110768098+Pankaj-Yaduvanshi@users.noreply.github.com>
2023-10-18 22:48:32 +0530

    Initial commit

--- src/main/java/assignment/synonymRegister/config/AppSecurityConfig.java
+++ src/main/java/assignment/synonymRegister/config/AppSecurityConfig.java
@@ -1,27 +1,27 @@
-package assignment.synonymRegister.config;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-@EnableWebSecurity
-public class AppSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return  http
-                .authorizeHttpRequests(authz -> {
-                    authz.requestMatchers("/api/fetch").hasAuthority("READER");
-                    authz.requestMatchers("/api/words").permitAll();
-                    authz.anyRequest().authenticated();
-                })
-                .oauth2Login(withDefaults())
-                .formLogin(withDefaults())
-                .build();
-    }
-}
+//package assignment.synonymRegister.config;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class AppSecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return  http
+//                .authorizeHttpRequests(authz -> {
+//                    authz.requestMatchers("/api/fetch").hasAuthority("READER");
+//                    authz.requestMatchers("/api/words").permitAll();
+//                    authz.anyRequest().authenticated();
+//                })
+//                .oauth2Login(withDefaults())
+//                .formLogin(withDefaults())
+//                .build();
+//    }
+//}
